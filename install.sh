4529431f251099ddc8c56fec10d91fa5751edbd2
Roy Matero <materoy@proton.me>
2023-12-02 16:42:39 -0600

    Serious commit

--- install.sh
+++ install.sh
@@ -3,9 +3,8 @@
 cross build  --target=armv7-unknown-linux-gnueabihf
 
 bin=target/armv7-unknown-linux-gnueabihf/debug/gc9a01-lcd-firmware
-pi_host="192.168.12.42"
 
 # Send the executable to raspberry pi
 echo "Sending executable to remote"
-scp $bin "$pi_host:"
+scp $bin "pi@pi:"
 echo "Sent ..."
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
+use rppal::spi::{Bus, Mode, Segment, SlaveSelect, Spi};
 
 // Instruction Set
 const WRITE: u8 = 0b0010;
@@ -7,13 +7,47 @@ const READ: u8 = 0b0011;
 const RDSR: u8 = 0b0101;
 const WREN: u8 = 0b0110;
 
+const WIP: u8 = 1;
+
 fn main() -> Result<(), Box<dyn Error>>{
 
     let mut spi = Spi::new(Bus::Spi0, SlaveSelect::Ss0, 8_000_000, Mode::Mode0)?;
 
-    // Read status
-    let result = spi.write(&[RDSR])?;
-    println!("{}", result);
+    // Set the write enable latch using the WREN instruction. This is required
+    // before any data can be written. The write enable latch is automatically
+    // reset after a WRITE instruction is successfully executed.
+    spi.write(&[WREN])?;
+
+    // Use the WRITE instruction to select memory address 0 and write 5 bytes
+    // (1, 2, 3, 4, 5). Addresses are specified as 24-bit values, but the 7 most
+    // significant bits are ignored.
+    spi.write(&[WRITE, 0, 0, 0, 1, 2, 3, 4, 5])?;
+
+    // Read the STATUS register by writing the RDSR instruction, and then reading
+    // a single byte. Loop until the WIP bit is set to 0, indicating the write
+    // operation is completed. transfer_segments() will keep the Slave Select line
+    // active until both segments have been transferred.
+    let mut buffer = [0u8; 1];
+    loop {
+        spi.transfer_segments(&[
+            Segment::with_write(&[RDSR]),
+            Segment::with_read(&mut buffer),
+        ])?;
+
+        if buffer[0] & WIP == 0 {
+            break;
+        }
+    }
+
+    // Use the READ instruction to select memory address 0, specified as a 24-bit
+    // value, and then read 5 bytes.
+    let mut buffer = [0u8; 5];
+    spi.transfer_segments(&[
+        Segment::with_write(&[READ, 0, 0, 0]),
+        Segment::with_read(&mut buffer),
+    ])?;
+
+    println!("Bytes read: {:?}", buffer);
 
     Ok(())
 }
