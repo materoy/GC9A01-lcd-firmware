1325282b547703cb6ec8073f16a3070d632a5e51
Roy Matero <materoy@proton.me>
2023-12-03 00:09:07 -0600

    Should display an image now if all is well

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,21 @@
 use std::error::Error;
+use std::thread::sleep;
+use std::time::Duration;
 use display_interface_spi::SPIInterface;
+use embedded_graphics::image::Image;
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
 use rppal::gpio::{Gpio, OutputPin};
 use rppal::pwm::Channel::Pwm0;
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
+use tinybmp::Bmp;
 
-// Instruction Set
-const WRITE: u8 = 0b0010;
-const READ: u8 = 0b0011;
-const RDSR: u8 = 0b0101;
-const WREN: u8 = 0b0110;
-
-const WIP: u8 = 1;
 
 // Pins
 const CS_PIN: u8 = 8;
 const DC_PIN: u8 = 25;
 const RST_PIN: u8 = 27;
-const BL_PIN: u8 = 18;
+const _BL_PIN: u8 = 18;
 
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -31,8 +28,9 @@ fn main() -> Result<(), Box<dyn Error>> {
     let rst_pin = gpio.get(RST_PIN)?.into_output();
 
     let spi_interface :SPIInterface<Spi, OutputPin, OutputPin> = SPIInterface::new(spi, dc, cs);
-
+    //
     let pwm = rppal::pwm::Pwm::new(Pwm0)?;
+    pwm.set_period(Duration::from_millis(30))?;
     pwm.enable()?;
 
     let mut display_driver = gc9a01a::GC9A01A::new(spi_interface, rst_pin, pwm);
@@ -45,5 +43,12 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     display_driver.clear(Rgb565::BLUE).unwrap();
 
+    let bmp : Bmp<Rgb565>= Bmp::from_slice(include_bytes!("rust.bmp")).unwrap();
+
+    let image = Image::new(&bmp, Point::new(56, 56));
+
+    image.draw(&mut display_driver).unwrap();
+
+    sleep(Duration::from_millis(2000));
     Ok(())
 }
