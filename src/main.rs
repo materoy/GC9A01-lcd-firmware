5f4007090bf573a4a825a656c4237fbd11955395
Roy Matero <materoy@proton.me>
2023-12-05 23:45:11 -0600

    Displays something at least

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,20 @@
 use std::error::Error;
-use std::thread::sleep;
 use std::time::Duration;
+
 use display_interface_spi::SPIInterface;
 use embedded_graphics::image::Image;
+use embedded_graphics::mono_font::ascii::FONT_6X10;
+use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::pixelcolor::Rgb565;
 use embedded_graphics::prelude::*;
+use embedded_graphics::primitives::{
+    Circle, CornerRadii, PrimitiveStyleBuilder, Rectangle, RoundedRectangle, StyledDrawable,
+};
+use embedded_graphics::text::Text;
 use rppal::gpio::{Gpio, OutputPin};
 use rppal::pwm::Channel::Pwm0;
 use rppal::spi::{Bus, Mode, SlaveSelect, Spi};
 use tinybmp::Bmp;
-
 
 // Pins
 const CS_PIN: u8 = 8;
@@ -17,6 +22,7 @@ const DC_PIN: u8 = 25;
 const RST_PIN: u8 = 27;
 const _BL_PIN: u8 = 18;
 
+const DISPLAY_SIZE: u32 = 240;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut delay = rppal::hal::Delay::new();
@@ -27,7 +33,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let cs = gpio.get(CS_PIN)?.into_output();
     let rst_pin = gpio.get(RST_PIN)?.into_output();
 
-    let spi_interface :SPIInterface<Spi, OutputPin, OutputPin> = SPIInterface::new(spi, dc, cs);
+    let spi_interface: SPIInterface<Spi, OutputPin, OutputPin> = SPIInterface::new(spi, dc, cs);
     //
     let pwm = rppal::pwm::Pwm::new(Pwm0)?;
     pwm.set_period(Duration::from_millis(30))?;
@@ -41,14 +47,47 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     display_driver.initialize(&mut delay).unwrap();
 
-    display_driver.clear(Rgb565::BLUE).unwrap();
+    display_driver.clear(Rgb565::BLACK).unwrap();
 
-    let bmp : Bmp<Rgb565>= Bmp::from_slice(include_bytes!("rust.bmp")).unwrap();
+    let bmp: Bmp<Rgb565> = Bmp::from_slice(include_bytes!("rust.bmp")).unwrap();
 
     let image = Image::new(&bmp, Point::new(56, 56));
 
     image.draw(&mut display_driver).unwrap();
 
-    sleep(Duration::from_millis(2000));
+    let style = PrimitiveStyleBuilder::new()
+        .stroke_width(4)
+        .stroke_color(Rgb565::GREEN)
+        .build();
+
+    // Outline Circle
+    Circle::new(Point::new(0, 0), DISPLAY_SIZE)
+        .draw_styled(&style, &mut display_driver)
+        .unwrap();
+
+    let style = PrimitiveStyleBuilder::new()
+        .stroke_width(2)
+        .stroke_color(Rgb565::GREEN)
+        .build();
+
+    let rectangle_width = 100;
+    let rec_x = (DISPLAY_SIZE - rectangle_width) / 2;
+    RoundedRectangle::new(
+        Rectangle::new(
+            Point::new(rec_x as i32, 20),
+            Size::new_equal(rectangle_width),
+        ),
+        CornerRadii::new(Size::new_equal(4)),
+    )
+    .draw_styled(&style, &mut display_driver)
+    .unwrap();
+
+    let text = "Hello Rust community";
+    let text_x = (DISPLAY_SIZE - (text.len() as u32 * 6)) / 2;
+    let character_style = MonoTextStyle::new(&FONT_6X10, Rgb565::WHITE);
+    Text::new(text, Point::new(text_x as i32, 190), character_style)
+        .draw(&mut display_driver)
+        .expect("TODO: panic message");
+
     Ok(())
 }
